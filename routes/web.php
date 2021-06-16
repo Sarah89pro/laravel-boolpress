a7b8cd5aeb131877beb782bfbf5a895f40f64062
Sarah89Pro <sarahproietti1@gmail.com>
2021-06-16 17:52:26 +0200

    feat migrate, admin route, homecontroller for adm

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('guest.welcome');
+});
+
+//AUTHENTICATION ROUTES
+Auth::routes();
+
+
+//ADMIN ROUTES
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin.')
+    ->group(function() {
+        //home admin route
+        Route::get('/admin', 'HomeController@index')->name('home');
+
+        //resource post route
+
+    });
+
