f2121f5bb64bd8ff28136e9e8914255b783e5b5b
Sarah89Pro <sarahproietti1@gmail.com>
2021-06-18 16:04:48 +0200

    feat js confim

---
+++ resources/js/admin.js
@@ -0,0 +1,17 @@
+require('./bootstrap');
+
+//confirmation request for every single post
+const Form = document.querySelectorAll('delete-post-form');
+//console.log('Form'); array so Foreach!
+
+Form.forEach( form=>{
+    form.addEventListener('submit', function(event) {
+        const resp = confirm('Do you really want to delete this post?');
+        
+        //console.log('resp');
+
+        if(!resp) {
+            event.preventDefault();
+        }
+    });
+});
