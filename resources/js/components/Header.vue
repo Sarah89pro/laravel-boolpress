a2ea0b8227695316c8bbde3e1e49d797202cf305
Sarah89Pro <sarahproietti1@gmail.com>
2021-06-27 20:12:35 +0200

    feat Header dynamic links

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
     <header>
-        <a href="">Boolpress</a>
 
         <nav>
             <ul>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">About</a></li>
+                <li><router-link :to="{name: 'home'}">Home</router-link></li>
+                <li><router-link :to="{name: 'about'}">About</router-link></li>
+                <li><router-link :to="{name: 'blog'}">Blog</router-link></li>
             </ul>
         </nav>
     </header>
